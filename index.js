376c90a61754573d653ce313b4d215f7c1a34e84
Roisin <roisin.benson@webelevate.ie>
2017-11-10 17:16:56 +0000

    made a change

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
